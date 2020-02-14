0a9f3de82b9d2b3e94348357577ab1619b3d9342
Zhou <gentle_zhou_shen@outlook.com>
2020-02-14 14:51:03 -0500

    Update encodeVideoFiles.py

--- Task1/encodeVideoFiles.py
+++ Task1/encodeVideoFiles.py
@@ -5,12 +5,16 @@ import subprocess
 os.chdir('.')
 
 # convert jpg to mp4
-subprocess.call(['ffmpeg', '-i', 'earth.jpg', 'output.mp4'])
+# ffmpeg -i img-%02d.png video_name.avi #2-digit number for name
+# set frame rate
+subprocess.call(['ffmpeg', '-framerate', '15', '-i', '%01d.jpg', 
+  'normal.avi'])
+
 # command line: 
 # ffmpeg -i input.mp4 -c:a copy -c:v copy -r 30 -s hd720 -b:v 2M output.mp4
-subprocess.call(['ffmpeg', '-i', 'output.mp4', '-c:a', 'copy', 
+subprocess.call(['ffmpeg', '-i', 'normal.avi', '-c:a', 'copy', 
   '-c:v', 'copy', '-r', '30', '-s', 'hd720', '-b:v', '2M', 
-  'test.mp4'])
+  'better.mp4'])
 
 
 # convert mp4 to gif
